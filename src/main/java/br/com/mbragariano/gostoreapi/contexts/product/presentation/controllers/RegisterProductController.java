a30b3d554e2bbb37bc59b3e5bf6f83c61f80696e
Matheus Braga <mbraga@grupodimed.com.br>
2021-05-05 12:50:18 -0300

    refactor(RegisterProduct): altera referência de handler no controller
    - Com a utilização da interface qualquer implementação do handler pode ser injetada, anteriormente com uma classe concreta estava limitado para aquela implementação.

--- src/main/java/br/com/mbragariano/gostoreapi/contexts/product/presentation/controllers/RegisterProductController.java
+++ src/main/java/br/com/mbragariano/gostoreapi/contexts/product/presentation/controllers/RegisterProductController.java
@@ -2,9 +2,11 @@ package br.com.mbragariano.gostoreapi.contexts.product.presentation.controllers;
 
 import br.com.mbragariano.gostoreapi.contexts.product.implementation.services.RegisterProductService;
 import br.com.mbragariano.gostoreapi.contexts.product.presentation.handlers.failures.concrete.DuplicatedProductNameFailureHandler;
+import br.com.mbragariano.gostoreapi.contexts.product.presentation.handlers.failures.contract.RegisterProductFailureHandler;
 import br.com.mbragariano.gostoreapi.contexts.product.presentation.mappers.RegisterProductPresentationMapper;
 import br.com.mbragariano.gostoreapi.contexts.product.presentation.models.requests.RegisterProductRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -16,12 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequiredArgsConstructor
 public class RegisterProductController {
 
   private final RegisterProductService registerProductService;
 
-  private final DuplicatedProductNameFailureHandler duplicatedProductNameFailureHandler;
+  private final RegisterProductFailureHandler registerProductFailureHandler;
+
+  public RegisterProductController(
+    RegisterProductService registerProductService,
+    @Qualifier("duplicatedProductNameFailureHandler") RegisterProductFailureHandler registerProductFailureHandler
+  ) {
+    this.registerProductService = registerProductService;
+    this.registerProductFailureHandler = registerProductFailureHandler;
+  }
 
   @PostMapping("/products")
   public ResponseEntity<?> handle(@RequestBody RegisterProductRequest registerProductRequest) {
@@ -29,7 +38,7 @@ public class RegisterProductController {
     final var entityOrFailure = this.registerProductService.execute(registerProductDto);
 
     if (entityOrFailure.isLeft())
-      return this.duplicatedProductNameFailureHandler.handle(entityOrFailure.getLeft());
+      return this.registerProductFailureHandler.handle(entityOrFailure.getLeft());
 
     final var registerProductResponse = RegisterProductPresentationMapper.mapToResponse(entityOrFailure.get());
 
